a5d6f363018fc5cb95b2cd777e5f67f76478dcba
lisamazzini <lisa.mazzini5@gmail.com>
2017-05-19 15:06:16 +0200

    ciao

--- frontend/src/map/Map.vue
+++ frontend/src/map/Map.vue
@@ -2,119 +2,98 @@
 <div id="main">
   <div id="container">
     <div id="map"></div>
-    
+    <button id="showList" v-on:click="showList">LIST</button>
+      <button id="showInst" v-on:click="showInst">instructions</button>
+     <div id="list">  
+      <ul>
+      <li v-for="item in poilist.list" v-on:click="showDetails(item)">
+        {{item.properties.nome}} - {{item.properties.provincia}}  
+      </li>   
+      </ul>
+    </div>
     <div id="instructions"></div>
 
-  </div>
-    
-  <div id="list">
-    <ul>
-    <li v-for="item in poilist.list">
-      {{item.properties.nome}} - {{item.properties.provincia}}  
-    </li>   
-    </ul>
   </div>
 
 </div>
 </template>
 <script>
+
 export default {
   props: ['poilist'],  
   name: 'main',
   data () {
     return {
-        // points : []
+
     }
   },
   methods: {
     initMap: function(){
-      
-    var point1 = {
-                  "_id" : "1",
-                  "id" : "19039",
-                  "properties" : {
-                      "nome" : "Chiesa della niente",
-                      "provincia" : "Bologna",
-                      "comune" : "MONZUNO",
-                      "frazione" : "Campagne",
-                      "tipologie_cronologie" : "Chiesa",
-                      "categoria" : "Edifici religiosi",
-                      "tipologia" : "Chiesa",
-                      "eta" : ""
-                  },
-                  "coordinates" : [
-                      12.2418846,
-                      44.1394082,
-                  ]   
-              };
-
-    var point2 = {
-                  "_id" : "1",
-                  "id" : "19039",
-                  "properties" : {
-                      "nome" : "San Mercuriale",
-                      "provincia" : "Forlì",
-                      "comune" : "MONZUNO",
-                      "frazione" : "Campagne",
-                      "tipologie_cronologie" : "Chiesa",
-                      "categoria" : "Edifici religiosi",
-                      "tipologia" : "Chiesa",
-                      "eta" : ""
-                  },
-                  "coordinates" : [
-                      12.2430853,
-                      44.1383596
-                  ]   
-              };
-
-      var point3 = {
-                  "_id" : "1",
-                  "id" : "19039",
-                  "properties" : {
-                      "nome" : "Torre Eiffel",
-                      "provincia" : "Cesena",
-                      "comune" : "MONZUNO",
-                      "frazione" : "Campagne",
-                      "tipologie_cronologie" : "Chiesa",
-                      "categoria" : "Edifici religiosi",
-                      "tipologia" : "Chiesa",
-                      "eta" : ""
-                  },
-                  "coordinates" : [
-                     12.243359,
-                     44.140697
-                  ]   
-              };          
-
-    // this.poilist.list = [point1, point2, point3];
-    
+              
+    if(this.poilist.list.length == 0){
+        
+    }
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
-        // center: new google.maps.LatLng(44.1400338,12.2412768),
         center: new google.maps.LatLng(this.poilist.list[0].coordinates[1], this.poilist.list[0].coordinates[0]),
         mapTypeId: 'terrain'
     });
     
     var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+    var directionsDisplay = new google.maps.DirectionsRenderer({map: map, suppressMarkers : true});
     directionsDisplay.setMap(map);
     directionsDisplay.setPanel(document.getElementById('instructions'));
 
-    
+    var infowindow = new google.maps.InfoWindow();
     var control = document.getElementById('container');
+    var extract;
+    var url;
     control.style.display = "block";
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
     var firstPoint = new google.maps.LatLng(this.poilist.list[0].coordinates[1], this.poilist.list[0].coordinates[0]);
     for (var i = 0; i < this.poilist.list.length; i++) {
-        var latLng = new google.maps.LatLng(this.poilist.list[i].coordinates[1], this.poilist.list[i].coordinates[0]);
-        var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
+    var name = this.poilist.list[i].properties.nome;
+    var latLng = new google.maps.LatLng(this.poilist.list[i].coordinates[1], this.poilist.list[i].coordinates[0]);
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        label: 'A'
+    }); 
+      var self = this
+      google.maps.event.addListener(marker, 'click', function(name) { 
+        return function(){
+        map.panTo(marker.getPosition());
+        console.log(marker.getPosition());    
+        self.$http.get('//it.wikipedia.org/w/api.php?action=query&format=json&prop=info|extracts&titles=' + name + ' &inprop=url&intestactions=&origin=*').then(response => {
+          var json = JSON.parse(JSON.stringify(response.body));
+          var id = Object.keys(response.body.query.pages)[0];
+           infowindow.setContent('<div id="content">'+
+            '<h1 id="firstHeading" class="firstHeading">' + name + ' </h1>');
+            
+          infowindow.open(map, this); 
+          //Se id è a -1 la pagina non esiste
+          if(id != -1){  
+            var object =  json["query"]["pages"][id];
+            url = object["canonicalurl"];
+            extract = object["extract"];
+             infowindow.setContent('<div id="content">'+
+            '<h1 id="firstHeading" class="firstHeading">' + name + ' </h1>'+
+            '<div id="bodyContent">'+
+            extract +
+            url + 
+            ''+
+            '</div>'+
+            '</div>');
+            infowindow.open(map, this); 
+          }
         });
+        }
+      }(name));
+      this.poilist.list[i].marker = marker;
     }
 
-    var waypoints = []
+    var waypoints = [] 
     for(var i = 1; i < this.poilist.list.length; i++){
       waypoints.push({
           location: new google.maps.LatLng(this.poilist.list[i].coordinates[1], this.poilist.list[i].coordinates[0]),
@@ -131,6 +110,7 @@ export default {
           // Route the directions and pass the response to a function to create
           // markers for each step.
           if (status === 'OK') {
+            directionsDisplay.set
             directionsDisplay.setDirections(response);
             
           } else {
@@ -138,20 +118,32 @@ export default {
           }
         });
      
-     //Cerco per ciascun punto se esiste la pagina di wikipedia
-     for(var i = 0; i < this.poilist.list.length; i++){
-       this.$http.get('//it.wikipedia.org/w/api.php?action=query&format=json&prop=info|extracts&titles=' + this.poilist.list[i].properties.nome + ' &inprop=url&intestactions=&origin=*').then(response => {
+   },
+  showList : function() {
+    document.getElementById("list").style.visibility='visible';
+    document.getElementById("instructions").style.visibility='hidden';
+  },
+  showInst : function() {
+    document.getElementById("list").style.visibility='hidden';
+    document.getElementById("instructions").style.visibility='visible';
+  },
+
+  showDetails: function(item){
+    console.log(item.properties.nome);
+    this.$http.get('//it.wikipedia.org/w/api.php?action=query&format=json&prop=info|extracts&titles=' + item.properties.nome + ' &inprop=url&intestactions=&origin=*').then(response => {
           var json = JSON.parse(JSON.stringify(response.body));
           var id = Object.keys(response.body.query.pages)[0];
-          //Se id è a -1 la pagina non esiste
+          console.log(id);
           if(id != -1){
+            console.log("ciao");
             var object =  json["query"]["pages"][id];
-            var url = object["canonicalurl"];
-            var extract = object["extract"];
+            url = object["canonicalurl"];
+            extract = object["extract"];
+            console.log(url);
           }
-       });
-     }
-  } 
+        });
+    }
+
   },
   computed: function() {
   },
@@ -183,11 +175,18 @@ export default {
     font-family: 'Roboto','sans-serif';
     line-height: 30px;
     padding-left: 10px;
+    visibility : hidden;
   }
 
   #list {
     width : 1300 px;
     height : 1000 px;
     position : relative;
+    visibility : hidden;
   }
+
+  li {
+    background-color : yellow;
+  }
+
 </style>
