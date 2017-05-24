4e9d3a7ff4be29b8b63db0b2fc3d42e9213254fd
albertogiunta <alb.giunta@gmail.com>
2017-05-24 21:58:55 +0200

    niente, come non detto. ciao lisa, ciao

--- frontend/src/search/SelectPOIs.vue
+++ frontend/src/search/SelectPOIs.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="selectpois">
     <div class="pois-container">
-    <!-- metodo 1 -->
-      <!--<div v-for="poi in pois" v-bind:class="[poi.is_selected ? 'selected' : '', 'poi']" @click="clickOnPoi(poi.id_cicer1)">-->
-    <!-- metodo 2 -->
-      <div v-for="poi in pois_ar" v-bind:class="[poi.is_selected ? 'selected' : '', 'poi']" @click="clickOnPoi(poi.id_cicer1)">
+      <div v-for="poi in pois" v-bind:class="[poi.is_selected ? 'selected' : '', 'poi']" @click="clickOnPoi(poi.id_cicer1)">
           <div class="left">
             <div class="name">
               {{poi.properties.nome}}
@@ -35,41 +32,25 @@ export default {
   name: 'selectpois',
   data () {
     return {
-      pois: this.poilist.list, //metodo 1
-      pois_ar: [] // metodo 2
+      pois: []
     }
   },
   methods: {
     addFields: function() {
-      for (var i = 0; i < this.pois.length; i++) {
-
-        // 1
-        this.pois[i].is_selected = true
-        this.pois[i].id_cicer1 = i
-
-        // 2
-        this.pois_ar.push(this.pois[i])
-      
-      } 
+      this.pois = JSON.parse(JSON.stringify(this.pois))
     },
     clickOnPoi: function(poi_id) {
       console.log(poi_id, this.pois[poi_id].is_selected, !this.pois[poi_id].is_selected)
-      // 1
-      // this.pois[poi_id].is_selected = !this.pois[poi_id].is_selected
-      // 1 bis
-      // this.poilist.list[poi_id].is_selected = !this.poilist.list[poi_id].is_selected
-      
-      // 2
-      this.pois_ar[poi_id].is_selected = !this.pois_ar[poi_id].is_selected
+      this.pois[poi_id].is_selected = !this.pois[poi_id].is_selected
     },
     goToMap: function() {
-    //   this.poilist.list = []
-    //   for (var i = 0; i < this.pois.length; i++) {
-    //     if (this.pois[i].is_selected) {
-    //         this.poilist.list.push(this.pois[i])
-    //     }
-    //   }
-    //   this.$router.push("/map")
+      this.poilist.list = []
+      for (var i = 0; i < this.pois.length; i++) {
+        if (this.pois[i].is_selected) {
+            this.poilist.list.push(this.pois[i])
+        }
+      }
+      this.$router.push("/map")
     }
   },
   watch: {
