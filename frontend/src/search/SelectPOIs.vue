1b822a398d9b6481b8b996d20028c7ed1185a432
albertogiunta <alb.giunta@gmail.com>
2017-05-24 21:20:37 +0200

    lisa è la tua volta, facci vedere quanto vali

--- frontend/src/search/SelectPOIs.vue
+++ frontend/src/search/SelectPOIs.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="selectpois">
     <div class="pois-container">
-      <div v-for="poi in pois">
-        <div class="poi" @click="clickOnPoi(poi.id_cicer1)">
-          <div v-bind:class="[poi.is_selected ? 'selected' : '', '']">
-            <div class="left">
-              <div class="name">
-                {{poi.properties.nome}}
-              </div>
-              <span class="type">
-                {{poi.properties.tipologia}}
-              </span>
+    <!-- metodo 1 -->
+      <!--<div v-for="poi in pois" v-bind:class="[poi.is_selected ? 'selected' : '', 'poi']" @click="clickOnPoi(poi.id_cicer1)">-->
+    <!-- metodo 2 -->
+      <div v-for="poi in pois_ar" v-bind:class="[poi.is_selected ? 'selected' : '', 'poi']" @click="clickOnPoi(poi.id_cicer1)">
+          <div class="left">
+            <div class="name">
+              {{poi.properties.nome}}
             </div>
-            <div class="right">
-              <div class="rating">
-                {{poi.rating}} ⭐️
-              </div>
-              <div class="popularity">
-                {{poi.popularity}} ✔️
-              </div>
+            <span class="type">
+              {{poi.properties.tipologia}}
+            </span>
+          </div>
+          <div class="right">
+            <div class="rating">
+              {{poi.rating}} ⭐️
+            </div>
+            <div class="popularity">
+              {{poi.popularity}} ✔️
             </div>
           </div>
-        </div>
       </div>
       <div class="cta-buttons-container">
         <div  @click="goToMap()" class="cta-button">🔎 Vai alla mappa</div>
@@ -36,35 +35,46 @@ export default {
   name: 'selectpois',
   data () {
     return {
-      pois: this.poilist.list
+      pois: this.poilist.list, //metodo 1
+      pois_ar: [] // metodo 2
     }
   },
   methods: {
     addFields: function() {
       for (var i = 0; i < this.pois.length; i++) {
-        this.pois[i].is_selected = false
+
+        // 1
+        this.pois[i].is_selected = true
         this.pois[i].id_cicer1 = i
+
+        // 2
+        this.pois_ar.push(this.pois[i])
+      
       } 
     },
     clickOnPoi: function(poi_id) {
       console.log(poi_id, this.pois[poi_id].is_selected, !this.pois[poi_id].is_selected)
-      this.pois[poi_id].is_selected = !this.pois[poi_id].is_selected
+      // 1
+      // this.pois[poi_id].is_selected = !this.pois[poi_id].is_selected
+      // 1 bis
+      // this.poilist.list[poi_id].is_selected = !this.poilist.list[poi_id].is_selected
+      
+      // 2
+      this.pois_ar[poi_id].is_selected = !this.pois_ar[poi_id].is_selected
     },
     goToMap: function() {
-      this.poilist.list = []
-      for (var i = 0; i < this.pois.length; i++) {
-        if (this.pois[i].is_selected) {
-            this.poilist.list.push(this.pois[i])
-        }
-      }
-      this.$router.push("/map")
+    //   this.poilist.list = []
+    //   for (var i = 0; i < this.pois.length; i++) {
+    //     if (this.pois[i].is_selected) {
+    //         this.poilist.list.push(this.pois[i])
+    //     }
+    //   }
+    //   this.$router.push("/map")
     }
   },
   watch: {
   },
   created() {
-    this.cose="super vaffanculo"
-    console.log("created")
     this.addFields()
   } 
 }
