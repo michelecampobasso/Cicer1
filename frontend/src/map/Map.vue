81b640da58c4658aea11ba7e5167d0f879034fdc
lisamazzini <lisa.mazzini5@gmail.com>
2017-06-19 11:46:56 +0200

    minor fix

--- frontend/src/map/Map.vue
+++ frontend/src/map/Map.vue
@@ -192,10 +192,8 @@ export default {
           // Route the directions and pass the response to a function to create
           // markers for each step.
           if (status === 'OK') {
-            directionsDisplay.set
             directionsDisplay.setDirections(response);
-            google.maps.event.trigger(map, 'resize');
-            
+            google.maps.event.trigger(map, 'resize')
           } else {
             window.alert('Directions request failed due to ' + status);
             this.$router.push('/');
@@ -322,7 +320,6 @@ export default {
     } else {
       this.initTags()   
       this.initMap()
-
     }
   }, 
   created() {
